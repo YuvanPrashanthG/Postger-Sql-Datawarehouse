0ab657e24b745bcc77727cf0951ffb7339a97295
Yuvan Prashanth G <133583959+YuvanPrashanthG@users.noreply.github.com>
2025-06-16 21:54:42 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,82 +9,53 @@ Script Purpose:
 ===============================================================================
 */
 
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
-GO
-
-CREATE TABLE bronze.crm_cust_info (
-    cst_id              INT,
-    cst_key             NVARCHAR(50),
-    cst_firstname       NVARCHAR(50),
-    cst_lastname        NVARCHAR(50),
-    cst_marital_status  NVARCHAR(50),
-    cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE
+drop table if exists bronz_layer.crm_cust_info;
+create table bronz_layer.crm_cust_info(
+	cst_id int,
+	cst_key varchar(50),
+	cst_firstname varchar(50),
+	cst_lastname varchar(50),
+	cst_marital_status varchar(50),
+	cut_gndr varchar(50),
+	cst_create_date date
 );
-GO
-
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info;
-GO
-
-CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+drop table if exists bronz_layer.crm_prd_info;
+create table bronz_layer.crm_prd_info(
+	prd_id int,
+	prd_key varchar(50),
+	prd_nm varchar(50),
+	prd_cost int,
+	prd_line varchar(50),
+	prd_start_dt date,
+	prd_end_dt date
 );
-GO
-
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details;
-GO
-
-CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+drop table if exists bronz_layer.crm_salse_details;
+create table bronz_layer.crm_salse_details(
+	sls_ord_num varchar(50),
+	sls_prd_key varchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
 );
-GO
-
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
-GO
-
-CREATE TABLE bronze.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+drop table if exists bronz_layer.erp_cust_az12;
+create table bronz_layer.erp_cust_az12(
+	cid varchar(50),
+	bdate date,
+	gen varchar(50)
 );
-GO
-
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12;
-GO
-
-CREATE TABLE bronze.erp_cust_az12 (
-    cid    NVARCHAR(50),
-    bdate  DATE,
-    gen    NVARCHAR(50)
+drop table if exists bronz_layer.erp_loc_a101;
+create table bronz_layer.erp_loc_a101(
+	cid varchar(50),
+	cntry varchar(50)
 );
-GO
-
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
-GO
-
-CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+drop table if exists bronz_layer.erp_px_cat_g1v2;
+create table bronz_layer.erp_px_cat_g1v2(
+	id varchar(50),
+	cat varchar(50),
+	subcat varchar(50),
+	maintenance varchar(50)
 );
-GO
