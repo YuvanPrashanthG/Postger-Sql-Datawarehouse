626ac616c6d3337c5bb36dd6454b9fbc2457bed2
Yuvan Prashanth G <133583959+YuvanPrashanthG@users.noreply.github.com>
2025-07-20 18:54:35 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,7 +9,7 @@ Script Purpose:
 ===============================================================================
 */
 
-drop table if exists bronz_layer.crm_cust_info;
+drop table if exists bronze.crm_cust_info;
 create table bronz_layer.crm_cust_info(
 	cst_id int,
 	cst_key varchar(50),
@@ -19,7 +19,7 @@ create table bronz_layer.crm_cust_info(
 	cut_gndr varchar(50),
 	cst_create_date date
 );
-drop table if exists bronz_layer.crm_prd_info;
+drop table if exists bronze.crm_prd_info;
 create table bronz_layer.crm_prd_info(
 	prd_id int,
 	prd_key varchar(50),
@@ -29,7 +29,7 @@ create table bronz_layer.crm_prd_info(
 	prd_start_dt date,
 	prd_end_dt date
 );
-drop table if exists bronz_layer.crm_salse_details;
+drop table if exists bronze.crm_salses_details;
 create table bronz_layer.crm_salse_details(
 	sls_ord_num varchar(50),
 	sls_prd_key varchar(50),
@@ -41,18 +41,18 @@ create table bronz_layer.crm_salse_details(
 	sls_quantity int,
 	sls_price int
 );
-drop table if exists bronz_layer.erp_cust_az12;
+drop table if exists bronze.erp_cust_az12;
 create table bronz_layer.erp_cust_az12(
 	cid varchar(50),
 	bdate date,
 	gen varchar(50)
 );
-drop table if exists bronz_layer.erp_loc_a101;
+drop table if exists bronze.erp_loc_a101;
 create table bronz_layer.erp_loc_a101(
 	cid varchar(50),
 	cntry varchar(50)
 );
-drop table if exists bronz_layer.erp_px_cat_g1v2;
+drop table if exists bronze.erp_px_cat_g1v2;
 create table bronz_layer.erp_px_cat_g1v2(
 	id varchar(50),
 	cat varchar(50),
