9fd6f761dbfd8d284840cd7e1c42e5ad541ef2ad
Yuvan Prashanth G <133583959+YuvanPrashanthG@users.noreply.github.com>
2025-07-20 19:43:36 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,12 +3,12 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    This script creates a new database named 'Warehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    Running this script will drop the entire 'Warehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
@@ -17,16 +17,16 @@ WARNING:
 -- Note: Requires superuser privileges to terminate connections
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'datawarehouse' AND pid <> pg_backend_pid();
+WHERE datname = 'Warehouse' AND pid <> pg_backend_pid();
 
-DROP DATABASE IF EXISTS datawarehouse;
+DROP DATABASE IF EXISTS Warehouse;
 
 -- Step 2: Create the new database
-CREATE DATABASE datawarehouse;
+CREATE DATABASE Warehouse;
 
 -- Step 3: Connect to the new database and create schemas
--- You need to connect to 'datawarehouse' before running the following:
-\c datawarehouse
+-- You need to connect to 'Warehouse' before running the following:
+\c Warehouse
 
 -- Now create schemas
 CREATE SCHEMA bronze;
