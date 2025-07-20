129d0eb0484e561aae55829ca3d13fe250063547
Yuvan Prashanth G <133583959+YuvanPrashanthG@users.noreply.github.com>
2025-07-20 19:00:30 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -16,7 +16,7 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
-CREATE OR REPLACE PROCEDURE bronz_layer.load_bronze()
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
 AS $$
 DECLARE
@@ -37,9 +37,9 @@ BEGIN
     -- Load crm_cust_info
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
-    TRUNCATE TABLE bronz_layer.crm_cust_info;
+    TRUNCATE TABLE bronze.crm_cust_info;
     RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
-    COPY bronz_layer.crm_cust_info
+    COPY bronze.crm_cust_info
     FROM 'C:/datasets/source_crm/cust_info.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -48,9 +48,9 @@ BEGIN
     -- Load crm_prd_info
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.crm_prd_info';
-    TRUNCATE TABLE bronz_layer.crm_prd_info;
+    TRUNCATE TABLE bronze.crm_prd_info;
     RAISE NOTICE '>> Inserting Data Into: bronze.crm_prd_info';
-    COPY bronz_layer.crm_prd_info
+    COPY bronze.crm_prd_info
     FROM 'C:/datasets/source_crm/prd_info.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -59,9 +59,9 @@ BEGIN
     -- Load crm_sales_details
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.crm_sales_details';
-    TRUNCATE TABLE bronz_layer.crm_sales_details;
+    TRUNCATE TABLE bronze.crm_sales_details;
     RAISE NOTICE '>> Inserting Data Into: bronze.crm_sales_details';
-    COPY bronz_layer.crm_sales_details
+    COPY bronze.crm_sales_details
     FROM 'C:/datasets/source_crm/sales_details.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -74,9 +74,9 @@ BEGIN
     -- Load erp_loc_a101
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.erp_loc_a101';
-    TRUNCATE TABLE bronz_layer.erp_loc_a101;
+    TRUNCATE TABLE bronze.erp_loc_a101;
     RAISE NOTICE '>> Inserting Data Into: bronze.erp_loc_a101';
-    COPY bronz_layer.erp_loc_a101
+    COPY bronze.erp_loc_a101
     FROM 'C:/datasets/source_erp/loc_a101.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -85,9 +85,9 @@ BEGIN
     -- Load erp_cust_az12
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.erp_cust_az12';
-    TRUNCATE TABLE bronz_layer.erp_cust_az12;
+    TRUNCATE TABLE bronze.erp_cust_az12;
     RAISE NOTICE '>> Inserting Data Into: bronze.erp_cust_az12';
-    COPY bronz_layer.erp_cust_az12
+    COPY bronze.erp_cust_az12
     FROM 'C:/datasets/source_erp/cust_az12.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -96,9 +96,9 @@ BEGIN
     -- Load erp_px_cat_g1v2
     start_time := NOW();
     RAISE NOTICE '>> Truncating Table: bronze.erp_px_cat_g1v2';
-    TRUNCATE TABLE bronz_layer.erp_px_cat_g1v2;
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
     RAISE NOTICE '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
-    COPY bronz_layer.erp_px_cat_g1v2
+    COPY bronze.erp_px_cat_g1v2
     FROM 'C:/datasets/source_erp/px_cat_g1v2.csv'
     WITH (FORMAT csv, HEADER true);
     end_time := NOW();
@@ -119,4 +119,4 @@ EXCEPTION
         RAISE NOTICE '==========================================';
 END;
 $$;
-CALL bronz_layer.load_bronze();
+CALL bronze.load_bronze();
